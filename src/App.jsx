8577a6327e991617601ce8338b904ca3d08def4a
Chiedozie Nkulo <dizzynkulo@gmail.com>
2025-05-30 20:31:04 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import NewProduct from './Products/Newproduct/Newproduct';
+import EditProduct from './Products/Editproduct/Editproduct';
 
 function App() {
-
   return (
-    <div className="text-4xl font-bold">
-        welcome
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/newproduct" replace />} />
+        <Route path="/newproduct/*" element={<NewProduct />} />
+        <Route path="/editproduct/*" element={<EditProduct />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
